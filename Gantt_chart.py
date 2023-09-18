f17fbc2004bb98cb6066e6b6f43d9bf87c31fd53
TFHVoe <510735@student.fontys.nl>
2023-09-18 21:44:09 +0200

    Buslijn in blokje gantt chart

--- Gantt_chart.py
+++ Gantt_chart.py
@@ -5,8 +5,8 @@ df = pd.read_excel("omloop planning.xlsx") #Load the data
 colors = {"materiaal rit":"darkorange", "dienst rit":"dodgerblue", "idle":"red", "opladen":"lime"} #Colors for the different boxes in the Gantt chart
 
 y_label = {'omloop nummer':'Circulation number'}
-fig = px.timeline(df, x_start="starttijd datum", x_end="eindtijd datum", y="omloop nummer", color = "activiteit",
-                  color_discrete_map = colors, labels = y_label, title = "Gantt chart circulation planning bus line 400 and 401") 
+fig = px.timeline(df, x_start="starttijd datum", x_end="eindtijd datum", y="omloop nummer", color = "activiteit", text="buslijn",
+                  color_discrete_map = colors, labels = y_label, title = "Gantt chart circulation planning bus line 400 and 401" ) 
 fig.update_yaxes(autorange="reversed") # Otherwise tasks are listed from the bottom up
 fig.update_layout(xaxis_title = "Period",
                  yaxis = dict(
