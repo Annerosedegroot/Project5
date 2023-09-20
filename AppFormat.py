d16d973a62a5f83c4b80dba64969b8bd49877ee9
Annerosedegroot <433534@student.fontys.nl>
2023-09-20 09:43:15 +0200

    Fout eruit halen zodat ik kan pullen

--- AppFormat.py
+++ AppFormat.py
@@ -7,15 +7,16 @@ import streamlit as st  # For creating a web application
 st.title("Project 5: Check the planning")
 
 # Planning in app zetten
-inputfile = st.file_uploader("Choose an Excel file", type='xlsx')
-df = pd.read_excel(inputfile)
+upload = st.file_uploader("Choose an Excel file", type='xlsx')
+
+if upload is not None:
+    df = pd.read_excel(upload, usecols= ['startlocatie', 'eindlocatie', 'starttijd',	'eindtijd',	'activiteit', 'buslijn', 'energieverbruik', 'starttijd datum', 'eindtijd datum', 'omloop nummer'])
+    st.table(df)
+
 # Lay-out app: sidebar
 add_selectbox = st.sidebar.selectbox(
     "How would you like to be contacted?",
     ("Email", "Home phone", "Mobile phone")
 )
-
-
-
 
 
