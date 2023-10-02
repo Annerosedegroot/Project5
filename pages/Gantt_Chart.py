cfaac03f7df1ec0dcf47e5dd88d240d7beb0e02e
Annerosedegroot <433534@student.fontys.nl>
2023-10-02 16:31:02 +0200

    paginas veranderd

--- pages/Gantt_Chart.py
+++ pages/Gantt_Chart.py
@@ -17,7 +17,7 @@ def Gantt_chart(df):
     colors = {"materiaal rit":"darkorange", "dienst rit":"dodgerblue", "idle":"red", "opladen":"lime"} #Colors for the different boxes in the Gantt chart
 
     y_label = {'omloop nummer':'Circulation number'}
-    fig = px.timeline(df, x_start="starttijd datum", x_end="eindtijd datum", y="omloop nummer", color = "activiteit", text="buslijn",
+    fig = px.timeline(df, x_start="starttijd datum", x_end="eindtijd datum", y="omloop nummer", color = "activiteit",
                   color_discrete_map = colors, labels = y_label, title = "Gantt chart circulation planning bus line 400 and 401" ) 
     fig.update_yaxes(autorange="reversed") # Otherwise tasks are listed from the bottom up
     fig.update_layout(xaxis_title = "Period",
