071cc5a62e43aa15060e0408b8bf3882d5b8d665
TFHVoe <510735@student.fontys.nl>
2023-10-15 21:00:35 +0200

    SOC check update

--- SOC_check.py
+++ SOC_check.py
@@ -5,9 +5,6 @@ import streamlit as st
 df = pd.read_excel("omloop planning.xlsx")
 df = df.drop(['Unnamed: 0'], axis=1)
 
-
-
-
 def SOC_check(df):
     """
     Function checks if there is a moment wen the State of Charge is violated    
@@ -30,19 +27,21 @@ def SOC_check(df):
     accu = original_capacity * SOH
     for i in range(len(df)):
         if df.iloc[i,9] in omloopnummer:
-            df.iloc[i,10] = df.iloc[i-1,10] - df.iloc[i,6]  # A new column gets added with the accu capacity for a moment.
+            df.iloc[i,10] = df.iloc[i-1,10] - df.iloc[i,6]  # A new column gets added with the accu capacity for a moment
         else:
             df.iloc[i,10] = accu - df.iloc[i,6]
-            omloopnummer.append(i)
-        
+            omloopnummer.append(i)   
+
     SOC_warning = []      
-    bool_SOC = df.iloc[:,10].lt(accu * SOC).to_list() 
+    bool_SOC = df.iloc[:,10].lt(accu * SOC).to_list()
     for i in range(len(bool_SOC)):
         if bool_SOC[i] == 1:
-            SOC_warning.append(f'Warning: In row {i} the SOC gets violated.')   # If a circulation numbers accu is less full than the SOC a warning is put out.
-    st.warning(SOC_warning)
+            if df.iloc[i, 9] == df.iloc[i+1, 9]: #If statement dat afdwingt dat het om hetzelfde omloop nummer gaat.
+                if df.iloc[i,4] == 'dienst rit' and df.iloc[i+1,4] == 'dienst rit':
+                    SOC_warning.append(f'Warning: In row {i} the SOC gets violated.') # If a circulation numbers accu is less full than the SOC a warning is put out.
 
-    df = df.drop('accu', axis=1)
+                elif df.iloc[i,4] == 'materiaal rit' and df.iloc[i+1,4] != 'idle' and df.iloc[i+1,4] != 'opladen':
+                    SOC_warning.append(f'Warning: In row {i} the SOC gets violated.') #If a circulation numbers accu is less full than the SOC a warning is put out
+                    continue
 
-    return SOC_warning, df
-    
+    return SOC_warning
