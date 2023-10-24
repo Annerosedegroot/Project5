8f521cbaf54581ac4816e7ff7291e668e4c71b8a
TFHVoe <t.voesenek@student.fontys.nl>
2023-10-24 10:07:39 +0200

    Idle time fill up verbeterd

--- Upload_data.py
+++ Upload_data.py
@@ -37,7 +37,7 @@ def checks(df, issues):
     time_backwards(df, issues)
     oplaadtijd(df, issues)
     SOC_check(df)
-    return issues
+    return issues, df
     
 # df = pd.read_excel('omloop planning.xlsx')
 
@@ -45,61 +45,12 @@ def checks(df, issues):
 st.title("Transdev Planning Checker")
 st.divider()
 
-#Input file 'planning' and 'Connexxion data', and check if 'planning' meets requirements
-# st.write('This tool is able to check circulation plannings for buslines 400 and 401 located in Eindhoven, the Netherlands.')
-# inputfile = st.file_uploader("Choose your completed circulation planning excel file", type='xlsx') # (to check if the format is correct)
-# issues = []
-# if inputfile is not None:
-#     df = pd.read_excel(inputfile)
-#     columnnames = df.columns.to_list()
-#     if 'Unnamed: 0' in columnnames:
-#         df = df.drop(['Unnamed: 0'], axis=1)
-#         st.session_state['df'] = df
-#         checks(df, issues)
-        
-#     else:
-#         checks(df, issues)
-#         st.session_state['df'] = df
-    
-
-
-#     #df.to_excel("Test2.xlsx")
-#     df_new = df.copy()  # A new dataframe such that the old one doesn't get overwritten when it is still needed.
-#     df_new = idle_time_fill_up(df_new)
-
-#     st.session_state['df_new'] = df_new
-    
-#     if not issues:
-#         st.success(f'Success: The file is correct.')
-#     upload2 = st.file_uploader("Choose the excel file which contains the requirements for the planning", type='xlsx')
-#     issues2 = []
-#     if upload2 is not None:
-#         df2 = pd.read_excel(upload2, sheet_name='Dienstregeling')
-#         check_omloop(df2, df, issues2) 
-#         st.success(f'Success: The file is correct.')
 
 
 #hieronder is de code voor de dropboxen langs elkaar, de oude staat nog gewoon hierboven gecomment.
 #Input file 'planning' and 'Connexxion data', and check if 'planning' meets requirements
 st.write('This tool is able to check circulation plannings for bus lines 400 and 401 located in Eindhoven, the Netherlands.')
 inputfile, inputfile2 = st.columns(2)
-
-# css = '''
-# <style>
-# [data-testid = "inputfile"] div div:: {content: "document alsjeblieft"}
-
-# </style>
-# '''
-# st.markdown(css, unsafe_allow_html=True)
-# st.markdown(
-#     """
-#     <style>
-#         .css-9ycgxx {
-#             display: "hoi ik ben Teun";
-#         }
-#     <style>
-#     """, unsafe_allow_html=True)
-
 
 inputfile = inputfile.file_uploader("Choose your completed circulation planning excel file", type='xlsx') # (to check if the format is correct)
 inputfile2 = inputfile2.file_uploader("Choose the excel file which contains the requirements", type='xlsx')
@@ -110,13 +61,12 @@ if inputfile is not None:
     if 'Unnamed: 0' in columnnames:
         df = df.drop(['Unnamed: 0'], axis=1)
         st.session_state['df'] = df
-        checks(df, issues)
+        issues,df = checks(df, issues)
         
     else:
-        checks(df, issues)
+        issues,df = checks(df, issues)
         st.session_state['df'] = df
     
-    #df.to_excel("Test2.xlsx")
     df = df.drop('accu', axis=1) # Deletes the column accu which is added in the SOC_check
     df_new = df.copy()  # A new dataframe such that the old one doesn't get overwritten when it is still needed.
     df_new = idle_time_fill_up(df_new)
@@ -131,11 +81,3 @@ if inputfile2 is not None:
     df2 = pd.read_excel(inputfile2, sheet_name='Dienstregeling')
     check_omloop(df2, df, issues2) 
     st.success(f'Success: The data file is correct.')
-
-
-
-#Reminder Teun Datum checks schrijft alle tijden uit!!!
-#Reminder Teun welk data frame te pakken voor SOC df of df_new!!
-
-
- 
--- idle_time_fill_up.py
+++ idle_time_fill_up.py
@@ -10,6 +10,24 @@ def idle_time_fill_up(df):
     df: DataFrame;
     Works with 'omloopplanning' xlsx file 
     """
+    #Starting time earlier than ending time - 1
+    lst = []
+    for j in range(len(df)-1):
+        if df.iloc[j+1,7] < df.iloc[j,8] and df.iloc[j,9] == df.iloc[j+1,9]:
+            lst.append(j+1)
+    df = df.drop(lst)
+    lst.clear()
+    df = df.reset_index(drop = True)
+    
+    #Starting time is endingtime
+    lst2 = []
+    for j in range(len(df)):
+        if df.iloc[j, 2] == df.iloc[j,3] :
+            lst2.append(j)
+    df = df.drop(lst2)
+    lst2.clear()
+    df = df.reset_index(drop = True)
+    
     IndexUnfilledIdleTimes = [] 
     for i in range(1, len(df.index)-1):
         if df.iloc[i,3] != df.iloc[i+1,2] and df.iloc[i,9] == df.iloc[i+1,9]: #Checks if there is a empty timeslot in a busline. 
@@ -17,7 +35,6 @@ def idle_time_fill_up(df):
 
     for i in IndexUnfilledIdleTimes:
         o = i +.5
-        #df.loc[o] =df.iloc[i,2], df.iloc[i,2], df.iloc[i,4], df.iloc[i+1,3], 'idle', np.NaN, 0.0100, df.iloc[i,9], df.iloc[i+1,8], df.iloc[i,10]
         df.loc[o] = df.iloc[i,1], df.iloc[i,1], df.iloc[i,3], df.iloc[i+1,2], 'idle', np.NaN, 0.0100, df.iloc[i,8], df.iloc[i+1,7], df.iloc[i,9] #Fills up the empty slot with the categorie idle time including the belonging properties. 
     df = df.sort_index().reset_index(drop=True)
     return df
