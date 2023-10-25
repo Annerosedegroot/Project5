95d3f7a7f0415a2d4b6ef84f83758e8c18032e36
TFHVoe <t.voesenek@student.fontys.nl>
2023-10-25 23:46:16 +0200

    Opladen van accu met betrekking van eerste 90% en laatste 10%.

--- SOC_check.py
+++ SOC_check.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+from datetime import datetime
 
 df = pd.read_excel("omloop planning.xlsx")
 df = df.drop(['Unnamed: 0'], axis=1)
@@ -21,16 +22,31 @@ def SOC_check(df):
     original_capacity = 300
     SOH = 0.85 
     SOC = 0.1
-
+    charging_speed_1 = 450 #kW/H
+    charging_speed_2 = 60 #kW/H
+    
     df['accu'] = np.zeros(len(df))
     omloopnummer = []
     accu = original_capacity * SOH
-    for i in range(len(df)):
-        if df.iloc[i,9] in omloopnummer:
-            df.iloc[i,10] = df.iloc[i-1,10] - df.iloc[i,6]  # A new column gets added with the accu capacity for a moment
+    for i in range(len(df)):     
+        if df.iloc[i,9] in omloopnummer:            
+            if df.iloc[i, 4] == 'opladen':
+                delta_t = datetime.strptime(df.iloc[i,3], "%H:%M:%S") - datetime.strptime(df.iloc[i,2], "%H:%M:%S")
+                charging_seconds = int(delta_t.total_seconds())
+                accu_charging_point = df.iloc[i-1,10]
+                charged = 0
+                for j in range(charging_seconds):
+                    if (accu_charging_point + charged) / accu < 0.9:
+                        charged += charging_speed_1 / 3600
+                    elif 0.9 < (accu_charging_point + charged) / accu < 1:
+                        charged += charging_speed_2 / 3600
+                df.iloc[i,6] = -charged
+                df.iloc[i,10] = df.iloc[i-1,10] - df.iloc[i,6] 
+            else:
+                df.iloc[i,10] = df.iloc[i-1,10] - df.iloc[i,6] # A new column gets added with the accu capacity for a moment
         else:
             df.iloc[i,10] = accu - df.iloc[i,6]
-            omloopnummer.append(i)   
+            omloopnummer.append(df.iloc[i,9])   
 
     SOC_warning = []      
     bool_SOC = df.iloc[:,10].lt(accu * SOC).to_list()
